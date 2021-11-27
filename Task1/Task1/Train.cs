792dcc7f5041e1a620a6087ec4aa92e35ec033cf
Fashington <valenmikubroccce@gmail.com>
2021-11-27 14:53:14 +0300

    Task 1 ready

---
+++ Task1/Task1/Train.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Train
+    {
+        private List<TrainCar> train = new List<TrainCar>();
+
+        public void AddTrainCar(int i, int j, string t)
+        {
+            train.Add(new TrainCar() { passengerСapacity = i, luggageCapacity = j, trainCarType = t });
+        }
+
+        public void RemoveTrainCar(int i)
+        {
+            train.RemoveAt(i);
+        }
+
+        public void ViewTrain()
+        {
+            foreach(TrainCar item in train)
+            {
+                Console.WriteLine("{0} | {1} | {2}", item.trainCarType, item.passengerСapacity, item.luggageCapacity);
+            }
+        }
+    }
+}
---
+++ Task1/Task1/TrainCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class TrainCar
+    {
+        public int passengerСapacity { get; set; }
+        public int luggageCapacity { get; set; }
+        public string trainCarType { get; set; }
+    }
+}
